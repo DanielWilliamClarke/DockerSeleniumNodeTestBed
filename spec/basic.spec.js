d41a536f6a580733400a2b96847e0dee45ecb972
Daniel Clarke <DClarke5@slb.com>
2018-11-08 11:50:07 +0000

    added docker compose support to setup selenium grid, added test to run on firefox

--- spec/basic.spec.js
+++ spec/basic.spec.js
@@ -1,19 +1,39 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const { expect } = require('chai');
 
-describe('DefaultTest', () => {
-    const driver = new Builder()
-    .forBrowser('chrome')
-    .usingServer("http://localhost:4444/wd/hub")
-    .build();
+describe("Basic Test", () => {
+    describe('Chrome Test', () => {
+        const driver = new Builder()
+            .forBrowser('chrome')
+            .usingServer("http://localhost:4444/wd/hub")
+            .build();
 
-    it('should go to nehalist.io and check the title', async () => {
-        await driver.get('http://www.google.com/ncr');
-        await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-        await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-        const title = await driver.getTitle();
-        expect(title).to.equal('webdriver - Google Search');
+        it('should go to google and check the title', async () => {
+            await driver.get('http://www.google.com/ncr');
+            await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+            await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+            const title = await driver.getTitle();
+            expect(title).to.equal('webdriver - Google Search');
+        });
+
+        after(async () => driver.quit());
     });
 
-    after(async () => driver.quit());
+    describe('FireFox Test', () => {
+        const driver = new Builder()
+            .forBrowser('firefox')
+            .usingServer("http://localhost:4444/wd/hub")
+            .build();
+
+        it('should go to google and check the title', async () => {
+            await driver.get('http://www.google.com/ncr');
+            await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+            await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+            const title = await driver.getTitle();
+            expect(title).to.equal('webdriver - Google Search');
+        });
+
+        after(async () => driver.quit());
+    });
 });
+
